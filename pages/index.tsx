00e13710ad169986de1212accdfbeb901a134bb1
LIK WAI NG <nglikwai@ymail.com>
2022-06-29 18:49:19 +0800

    fix: maximum-scale

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ const Home: NextPage<Props> = ({ recommandation = [] }) => {
         <title>
           HK01
         </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1 , minimum-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1 , maximum-scale=1" />
 
         <link rel='icon' href='/favicon.ico' />
       </Head>
